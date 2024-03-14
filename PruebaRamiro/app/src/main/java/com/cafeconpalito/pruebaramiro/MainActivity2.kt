a2ca3e0f177b4173f5dc36d55d21eff2776b60dc
Ramiro <112170316+ramirogvalverde@users.noreply.github.com>
2024-03-14 18:57:23 +0100

    hasta aquí por hoy

--- PruebaRamiro/app/src/main/java/com/cafeconpalito/pruebaramiro/MainActivity2.kt
+++ PruebaRamiro/app/src/main/java/com/cafeconpalito/pruebaramiro/MainActivity2.kt
@@ -2,10 +2,30 @@ package com.cafeconpalito.pruebaramiro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity2 : AppCompatActivity() {
+
+    lateinit var ivDoubleCheck: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        initUi()
+    }
+    private fun initVariables() {
+        ivDoubleCheck = findViewById(R.id.ivDoubleCheck)
+    }
+    private fun initUi() {
+        initVariables()
+        initListeners()
+    }
+    private fun initListeners() {
+        ivDoubleCheck.setOnClickListener {changeView()}
+    }
+
+    private fun changeView() {
+        intent.setClass(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
